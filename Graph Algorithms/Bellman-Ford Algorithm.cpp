2cf19e71e14e45d33b2a4ea72c85d484d1039a2c
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2024-07-17 12:21:38 +0530

    Update Bellman-Ford Algorithm.cpp

--- Graph Algorithms/Bellman-Ford Algorithm.cpp
+++ Graph Algorithms/Bellman-Ford Algorithm.cpp
@@ -1,23 +1,6 @@
-    //Can detect negative cycles
-
-    int size = number_of_nodes;
-    int start = starting_node;
-    int end = ending_node;
-
-    //edge list adjacency form where each edge is represented as (a, b, w) : starting node, ending node, weight/length of edge
-
-    vvl edges; 
-
-    //distance[x] = shortest distance of node x from starting node
-
-    vl distance(size + 1, inf);  
+void BellmanFord(vvl edges, vl& distance, int nodes, int start, int& r){
     distance[start] = 0;
-
-    // if number of nodes = n (size) : We do exactly n - 1 iterations
-    //Why n - 1 ? because this algorithm is based on bfs approach i.e. it first updates only those distance vector values whose distance form starting node is 1, then in the 
-    //next iteration it works for distance 2 and so on. Now this bfs approach would require n - 1 iterations in the worst case so as to explore all the levels
-
-    forl(i, 1, size){
+    forl(i, 0, nodes - 1){
         fora(x, edges){
             if (distance[x[1]] > distance[x[0]] + x[2]){
                 distance[x[1]] = distance[x[0]] + x[2];
@@ -25,16 +8,29 @@
         }
     }
 
-    //if it contains negative cycles then only it will update in this round
+    //Check for negative cycles
 
-    int a = 0;
     fora(x, edges){
         if (distance[x[1]] > distance[x[0]] + x[2]){
-            a++;
-            break;
+            r++;
         }
+        break;
     }
-    if (a) cout<<-1;
-    else cout<<distance[ending_node];
+}
+
+    //value of r tells us about negative cycles
+
+    //inputs
+    vl distance(nodes + 1, inf);  
+    int r = 0;
+
+    //edge adjacency form where each edge is represented as {a, b, w} : starting node, ending node, weight/length of edge
+    //distance[x] = shortest distance of node x from starting node
+
+    // if number of nodes = n (size) : We do exactly n - 1 iterations
+    //Why n - 1 ? because this algorithm is based on bfs approach i.e. it first updates only those nodes distances which are adjacent to the starting node, then in the 
+    //next iteration it works for neighbours of these nodes and so on. Now this bfs approach would require n - 1 iterations in the worst case so as to explore all the levels.
+
+    //if it contains negative cycles then only it will update in n-th round
 
     //Time complexity = O(n*m)
