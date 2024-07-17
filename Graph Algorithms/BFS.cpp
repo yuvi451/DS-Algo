71b4b4e82ba29346b39ba2399a975af380c097f9
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2024-07-17 11:26:58 +0530

    Update BFS.cpp

--- Graph Algorithms/BFS.cpp
+++ Graph Algorithms/BFS.cpp
@@ -1,28 +1,29 @@
-    int size = number_of_nodes
-    vl adj[size + 1]; // adjacency list  
-    vl visited(size + 1); 
-    vl ans;
-    int start = starting_node;
-    visited[start] = 1;
-    ans.pb(start);
+void BFS(vvl adj, vl& ans, int start){
+    int n = adj.size();
+    vl visited(n + 1);
+
     queue <int> q;
     q.push(start);
+    visited[start] = 1;
+    ans.pb(start);
+
     while (q.size() != 0){
         int node = q.front();
         q.pop();
+
         fora(x, adj[node]){
             if (!(visited[x])){
                 visited[x] = 1;
-                ans.pb(x);
                 q.push(x);
+                ans.pb(x);
             }
         }
     }
-
+}
 
   //Time Complexity = O(n + m) : every node and edge is visited only once
-  //ans vector stores bfs ordering; 
-  //BFS is a level by level search algorithm i.e. we first look for all elements that are at distance x from starting node and then only look for elements 
-  //that are at disatance x + 1 from start [Breadth/Horizontal First Search]
-  //Unlike DFS it picks up a neighbour (adjacent element), visits it and then goes on to the next neighbour (which is also at same distance from start)
+  //ans vector stores bfs ordering
+  //BFS is a level by level search algorithm i.e. we first look for all elements that are adjacent to the starting node and then only look for elements 
+  //that are at adjacent to the first neighbours of the starting node [Breadth/Horizontal First Search]
+  //Unlike DFS it picks up a neighbour (adjacent element), visits it and then goes on to the next neighbour (which is also adjacent to the start)
   
