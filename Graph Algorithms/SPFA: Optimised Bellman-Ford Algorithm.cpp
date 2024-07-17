3579b1b5da07c0fcad546b4c6b4d049950a32ddc
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2024-07-17 15:35:28 +0530

    Update SPFA: Optimised Bellman-Ford Algorithm.cpp

--- Graph Algorithms/SPFA: Optimised Bellman-Ford Algorithm.cpp
+++ Graph Algorithms/SPFA: Optimised Bellman-Ford Algorithm.cpp
@@ -1,49 +1,51 @@
-    //Works only for directed graph
-    int start = 1;
-    int end = 5;
-    int n = 5; //size of graph
-
-    //adjacency list: adj[a] = {{b, w}, {b1, w1}, ...} : all edges from node a, that end at b & have weight w
-
-    vvl adj[n + 1];
-    adj[1].pb({3, 3});
-    adj[1].pb({2, 5});
-    adj[1].pb({4, 7});
-    adj[2].pb({4, 3});
-    adj[2].pb({5, 2});
-    adj[3].pb({4, 1});
-    adj[4].pb({5, 2});
-
-    //ditance[x] = minimum distance of node x from starting node
-
-    vl distance(n + 1, inf);
+void SPFA(vvl edges, int nodes, vl& distance, int start, int&r){
     distance[start] = 0;
+    vector <vvl> adj(nodes + 1);
+    fora(x, edges){
+        adj[x[0]].pb({x[1], x[2]});
+    }
 
-    //here instead of a queue we use a deque. A deque is basically a faster queue.
+    deque<int>dq;
+    vl in_deque(nodes + 1);
+    vl update_count(nodes + 1, 0);
 
-    vl in_deque(n + 1);
-    deque <int> dq;
     dq.push_back(start);
     in_deque[start] = 1;
+    
 
     while (dq.size() != 0){
         int node = dq.front();
         dq.pop_front();
         in_deque[node] = 0;
-        
+
         fora(x, adj[node]){
-            if (!in_deque[x[0]]){
-                    if (!dq.empty() && distance[x[0]] < distance[dq.front()]){
-                        dq.push_front(x[0]);
-                    } else {
-                        dq.push_back(x[0]);
-                    }
-                    in_deque[x[0]] = 1;
+
+            distance[x[0]] = min(distance[x[0]], distance[node] + x[1]);
+            
+            if (!(in_deque[x[0]])){
+                if (!(dq.empty()) && distance[x[0]] < distance[dq.front()]){
+                    dq.push_front(x[0]);
+                } else {
+                    dq.push_back(x[0]);
                 }
-            distance[x[0]] = min(distance[x[0]], x[1] + distance[node]);
+                in_deque[x[0]] = 1;
+
+                update_count[x[0]]++;
+                if (update_count[x[0]] > nodes) {
+                    r = 1;
+                    return;
+                }
+            }
         }
     }
-    cout<<distance[end];
+}
+
+    //inputs
+    //vl distance(nodes + 1, inf);
+    //int r = 0;  for checking negative cycles (if r == 1)
+
+    //adjacency list: adj[a] = {{b, w}, {b1, w1}, ...} : all edges from node a, that end at b & have weight w
+    //here instead of a queue we use a deque. A deque is basically a faster queue.
 
     //This algorithm purely uses bfs approach. The Bellman-Ford algorithm also uses the bfs approach as it updates only those distance vector values in one go that lie at
     //a particular level/distance from the start. But in this process it examines all the edges, most of which are useless.
