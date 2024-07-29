79119b7fef581875285d3a1be0c2a7692e9901cf
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2024-07-29 22:35:55 +0530

    Update Prim's Algoritm.cpp

--- Graph Algorithms/Prim's Algoritm.cpp
+++ Graph Algorithms/Prim's Algoritm.cpp
@@ -1,1 +1,46 @@
+struct Compare {
+    bool operator()(const vl& a, const vl& b) {
+        return a[0] > b[0]; 
+    }
+};
 
+void PrimsAlgorithm(ll n, vvl edges, vvl& ans){
+    vl visited(n + 1);
+    ll mst = 0;
+    vector <vector<pr>>adj(n + 1);
+  
+    fora(x, edges){
+        adj[x[0]].pb({x[1], x[2]});
+        adj[x[1]].pb({x[0], x[2]});
+    }
+  
+    priority_queue <vl, vvl, Compare> pq;
+    pq.push({0, 0, -1});
+    // {weight, node, parent}
+  
+    while (!(pq.empty())){
+        vl v = pq.top();
+        pq.pop();
+        if (visited[v[1]]) continue;
+        visited[v[1]] = 1;
+        mst += v[0];
+        if (v[2] != -1){
+            ans.pb({v[2], v[1]});
+        }
+        fora(x, adj[v[1]]){
+            if (!(visited[x.first])){
+                pq.push({x.second, x.first, v[1]});
+            }
+        }
+    }
+    cout<<mst;
+}
+
+//inputs
+ll n;
+vvl ans;
+vvl edges;
+
+// intuition of this algorithm : Greedy 
+// Time complexity = O(n + m*log(m))
+//here forming the adjacency list also takes up some time. If it is already given then that time is savec
