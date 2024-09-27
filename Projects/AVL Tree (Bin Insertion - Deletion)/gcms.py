606d99166719b1b455119740fad2d2bb8b1ed431
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2024-09-27 15:38:31 +0530

    Update gcms.py

--- Projects/AVL Tree (Bin Insertion - Deletion)/gcms.py
+++ Projects/AVL Tree (Bin Insertion - Deletion)/gcms.py
@@ -13,32 +13,44 @@ def comp2(root, key):
         return 0
     return 1
 
+def comp3(root, key):
+    if root.key.bin_capacity > key.bin_capacity or (root.key.bin_capacity == key.bin_capacity and root.key.id < key.id):
+        return 0
+    return 1
+
 
 class GCMS:
     def __init__(self):
         self.Tree = AVLTree(comp1)
+        self.Tree1 = AVLTree(comp3)
         self.Object_ID = AVLTree(comp2)
         self.Bin_ID = AVLTree(comp2)
 
     def add_bin(self, bin_id, capacity):
         bin = Bin(bin_id, capacity)
         self.Tree.root = self.Tree.insert(self.Tree.root, bin)
+        self.Tree1.root = self.Tree1.insert(self.Tree1.root, bin)
         self.Bin_ID.root = self.Bin_ID.insert(self.Bin_ID.root, bin)
 
     def add_object(self, object_id, size, color):
         cargo = Object(object_id, size, color)
         node = None
-        node = self.Tree.Cargo(self.Tree.root, cargo, color, node)
+        if color == Color.BLUE or color == Color.GREEN:
+            node = self.Tree.Cargo(self.Tree.root, cargo, color, node)
+        else:
+            node = self.Tree1.Cargo(self.Tree1.root, cargo, color, node)
         if node is None:
             raise NoBinFoundException()
 
         bin = node.key
 
         self.Tree.root = self.Tree.delete(self.Tree.root, bin)
+        self.Tree1.root = self.Tree1.delete(self.Tree1.root, bin)
 
         bin.add_object(cargo)
 
         self.Tree.root = self.Tree.insert(self.Tree.root, bin)
+        self.Tree1.root = self.Tree1.insert(self.Tree1.root, bin)
         self.Object_ID.root = self.Object_ID.insert(self.Object_ID.root, cargo)
 
     def delete_object(self, object_id):
@@ -54,10 +66,12 @@ class GCMS:
             bin_1 = bin_node_1.key
 
             self.Tree.root = self.Tree.delete(self.Tree.root, bin_1)
+            self.Tree1.root = self.Tree1.delete(self.Tree1.root, bin_1)
 
             bin_1.remove_object(object_id)
 
             self.Tree.root = self.Tree.insert(self.Tree.root, bin_1)
+            self.Tree1.root = self.Tree1.insert(self.Tree1.root, bin_1)
 
             self.Object_ID.root = self.Object_ID.delete_1(self.Object_ID.root, obj_node.key)
 
@@ -70,4 +84,3 @@ class GCMS:
         a = self.Object_ID.search_1(self.Object_ID.root, object_id)
         if a is not None:
             return a.key.bin_id
-
