e3c25afb2bc0db1c74a7c1d543d415a626abd521
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2024-11-30 12:28:55 +0530

    Create flight.py

---
+++ Projects/Flight Planner/flight.py
@@ -0,0 +1,9 @@
+class Flight:
+    def __init__(self, flight_no, start_city, departure_time, end_city, arrival_time, fare):
+        self.flight_no = flight_no
+        self.start_city = start_city
+        self.departure_time = departure_time
+        self.end_city = end_city
+        self.arrival_time = arrival_time
+        self.fare = fare
+        
