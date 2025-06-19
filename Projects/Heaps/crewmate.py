af43533a35504d71add5fb16038d992c12eb99ff
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2025-06-19 21:45:34 +0530

    Update crewmate.py

--- Projects/Heaps/crewmate.py
+++ Projects/Heaps/crewmate.py
@@ -1,62 +1,44 @@
-from heap import *
-from treasure import *
+from heap import Heap
+from treasure import Treasure
 
-def max_heap(a, b):
-    if a.priority() > b.priority() or (a.priority() == b.priority() and a.id < b.id):
-        return True
+def comp(T1, T2):
+    p1 = -(T1.arrival_time + T1.remaining_size())
+    p2 = -(T2.arrival_time + T2.remaining_size())
+
+    if p1 > p2 or (p1 == p2 and T1.id < T2.id): return True
     return False
 
 class CrewMate:
 
-    def __init__(self, id):
-        self.id = id
-        self.treasures = []
-        self.l = []
+    def __init__(self, crew_id):
+        self.crew_id = crew_id
         self.load = 0
         self.last_load_time = 0
-        self.prev_time = 0
-        self.heap_t = Heap(max_heap, [])
+        self.pq = Heap(comp, [])    # All treasures in pq of crewmate are processed upto last_load_time of crewmate
+        self.processed_treasures = []
 
-    def process(self, hp, curr_time, prev_time, l):
-        while hp.top() and prev_time < curr_time:
-            a = hp.extract()
-            if prev_time + a.remaining_size() <= curr_time:
-                prev_time += a.remaining_size()
-                a.completion_time = prev_time
-                l.append(a)
+    def add(self, T):
+        curr_time = T.arrival_time
+        processing_units = curr_time - self.last_load_time
+        t = self.last_load_time
+
+        while self.pq.top() is not None and processing_units > 0:
+            Tr_top = self.pq.extract()
+
+            if processing_units >= Tr_top.remaining_size():
+                processing_units -= (Tr_top.remaining_size())
+                self.load -= (Tr_top.remaining_size())
+                t += (Tr_top.remaining_size())
+                Tr_top.completion_time = t
+                Tr_top.size = 0
+                self.processed_treasures.append(Tr_top)
+
             else:
-                a.processed_time += (curr_time - prev_time)
-                prev_time = curr_time
-                hp.insert(a)
+                Tr_top.size -= processing_units
+                self.load -= processing_units
+                processing_units = 0
+                self.pq.insert(Tr_top)
 
-
-    def add_t(self, trsr):
-        self.treasures.append(trsr)
-
-    def get_compltn_time(self):
-        for i in self.treasures:
-            self.process(self.heap_t, i.arrival_time, self.prev_time, self.l)
-            self.heap_t.insert(i)
-            self.prev_time = i.arrival_time
-
-        d = []
-        prev_time1 = self.prev_time
-        for i in self.heap_t.heap:
-            i = i.copy()
-            i.completion_time = i.remaining_size() + prev_time1
-            d.append(i)
-            prev_time1 = i.completion_time
-
-        self.treasures.clear()
-        return self.l + d
-
-
-
-
-
-
-
-
-
-
-
+        self.last_load_time = T.arrival_time
+        self.pq.insert(T)
+        self.load += T.size
