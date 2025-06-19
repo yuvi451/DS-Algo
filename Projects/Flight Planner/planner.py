8cac31108bf1fe07fbf30d7b5726448a2c79026c
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2025-06-19 12:39:05 +0530

    Update planner.py

--- Projects/Flight Planner/planner.py
+++ Projects/Flight Planner/planner.py
@@ -61,202 +61,166 @@ class PriorityQueue:
 
 
 class Graph:
-    def __init__(self, nodes):
-        self.nodes = nodes
-        self.adj = [[] for i in range(nodes + 1)]
+    def __init__(self, m):
+        self.adj = [[] for i in range(m + 1)]
 
-    def addEdge(self, a, b):
-        self.adj[a].append(b)
+    def addEdge(self, f1, f2):
+        self.adj[f1.flight_no].append(f2)
 
 
 class Planner:
     def __init__(self, flights):
         self.m = len(flights)
+        self.all_flights = [None for i in range(self.m + 1)]
+        self.start_city = [[] for i in range(self.m + 1)]
+        self.end_city = [[] for i in range(self.m + 1)]
         self.graph = Graph(self.m)
-        self.city_start, self.city_end, self.all_flights = [None] * (self.m + 1), [None] * (self.m + 1), [None] * (self.m + 1)
 
-        for flt in flights:
-            start_city = flt.start_city
-            end_city = flt.end_city
+        for i in flights:
+            self.all_flights[i.flight_no] = i
+            self.start_city[i.start_city].append(i)
+            self.end_city[i.end_city].append(i)
 
-            if self.city_start[start_city] is None:
-                self.city_start[start_city] = [flt]
-            else:
-                self.city_start[start_city].append(flt)
-
-            if self.city_end[end_city] is None:
-                self.city_end[end_city] = [flt]
-            else:
-                self.city_end[end_city].append(flt)
-
-            self.all_flights[flt.flight_no] = flt
-
-        for flight1 in self.all_flights:
-            if flight1:
-                end_city = flight1.end_city
-                atime = flight1.arrival_time
-                if self.city_start[end_city] is not None:
-                    for flight2 in self.city_start[end_city]:
-                        dtime = flight2.departure_time
-                        if dtime - atime >= 20:
-                            self.graph.addEdge(flight1.flight_no, flight2.flight_no)
+        for i in flights:
+            end_i = i.end_city
+            for j in self.start_city[end_i]:
+                if j.departure_time - i.arrival_time >= 20:
+                    self.graph.addEdge(i, j)
 
     def least_flights_ealiest_route(self, start_city, end_city, t1, t2):
-        path = []
+        list = [(1e9, 1e9, None) for i in range(self.m + 1)]  # (distance, t_arrival, parent)
+        route = []
 
-        if self.city_start[start_city] is not None and start_city != end_city:
-            for flt in self.city_start[start_city]:
-                if flt.departure_time >= t1 and flt.arrival_time <= t2:
-                    visited, distance = [False] * (self.m + 1), [(0, 0)] * (self.m + 1)    # (distance, previous_flight_object)
-                    visited[flt.flight_no] = True
-                    q = [flt.flight_no]
+        if start_city != end_city and t1 <= t2:
+            for i in self.start_city[start_city]:
+                if i.departure_time >= t1 and i.arrival_time <= t2:
+                    list[i.flight_no] = (0, i.arrival_time, None)
+                    visited = [False] * (self.m + 1)
+                    visited[i.flight_no] = True
+                    q = [i]
 
                     while q:
-                        curr_flight = q.pop(0)
+                        flight = q.pop(0)
 
-                        for flight in self.graph.adj[curr_flight]:
-                            next_flight_obj = self.all_flights[flight]
-                            next_flight_no = flight
-                            if not visited[next_flight_no] and next_flight_obj.departure_time >= t1 and next_flight_obj.arrival_time <= t2:
-                                visited[next_flight_no] = True
-                                q.append(next_flight_no)
-                                distance[next_flight_no] = (distance[curr_flight][0] + 1, self.all_flights[curr_flight])
+                        for next_flight in self.graph.adj[flight.flight_no]:
+                            if next_flight.arrival_time <= t2 and not visited[next_flight.flight_no]:
+                                visited[next_flight.flight_no] = True
+                                q.append(next_flight)
 
-                    t = (None, 1e9, 1e9)       # (flight_object, distance, time)
-                    if self.city_end[end_city]:
-                        for flgt in self.city_end[end_city]:
-                            if flgt.departure_time >= t1 and flgt.arrival_time <= t2:
-                                if distance[flgt.flight_no][1]:
-                                    if distance[flgt.flight_no][0] < t[1]:
-                                        t = (flgt, distance[flgt.flight_no][0], flgt.arrival_time)
-                                    elif distance[flgt.flight_no][0] == t[1] and flgt.arrival_time < t[2]:
-                                        t = (flgt, distance[flgt.flight_no][0], flgt.arrival_time)
-                                elif distance[flgt.flight_no][1] == 0 and flgt.start_city == start_city and flgt.arrival_time < t[2]:
-                                    t = (flgt, distance[flgt.flight_no][0], flgt.arrival_time)
+                                flt = list[next_flight.flight_no]
+                                par = list[flight.flight_no]
+                                if par[0] + 1 < flt[0]:
+                                    list[next_flight.flight_no] = (1 + par[0], next_flight.arrival_time, flight)
 
-                    if t[0] is not None:
-                        curr_path = []
-                        curr_flgt = t[0]
-                        while distance[curr_flgt.flight_no][1] != 0:
-                            curr_path.append(curr_flgt)
-                            curr_flgt = distance[curr_flgt.flight_no][1]
-                        curr_path.append(curr_flgt)
+            last_flight = (1e9, 1e9, None)
+            flt = None
+            for i in self.end_city[end_city]:
+                ft = list[i.flight_no]
+                if ft[0] < last_flight[0]:
+                    last_flight = ft
+                    flt = i
+                elif ft[0] == last_flight[0] and ft[1] < last_flight[1]:
+                    last_flight = ft
+                    flt = i
 
-                        curr_path.reverse()
-                        if (len(path) == 0) or (len(curr_path) < len(path)) or (len(path) == len(curr_path) and curr_path[-1].arrival_time < path[-1].arrival_time):
-                            path = curr_path
-        return path
+            if flt is not None:
+                route.append(flt)
+                while last_flight[2] is not None:
+                    route.append(last_flight[2])
+
+                    last_flight = list[last_flight[2].flight_no]
+
+        return route[::-1]
 
     def cheapest_route(self, start_city, end_city, t1, t2):
-        path = []
+        route = []
+        list = [(1e9, None) for i in range(self.m + 1)]   # (cost, parent)
 
-        if self.city_start[start_city] is not None and start_city != end_city:
-            for flt in self.city_start[start_city]:
-                if flt.departure_time >= t1 and flt.arrival_time <= t2:
-                    cost, processed = [(1e9, 1e9)] * (self.m + 1), [False] * (self.m + 1)
+        if start_city != end_city and t1 <= t2:
+            for i in self.start_city[start_city]:
+                if i.departure_time >= t1 and i.arrival_time <= t2:
+                    list[i.flight_no] = (i.fare, None)
+                    visited = [False] * (self.m + 1)
                     pq = PriorityQueue()
-                    cost[flt.flight_no] = (0, 0)
-                    pq.push((0, flt.flight_no))
+                    pq.push((i.fare, i.flight_no))
 
                     while not pq.is_empty():
-                        curr_flight = pq.top()[1]
-                        pq.pop()
+                        flight = pq.pop()
 
-                        if processed[curr_flight]: continue
-                        processed[curr_flight] = True
+                        if not visited[flight[1]]:
+                            visited[flight[1]] = True
+                            for next_flight in self.graph.adj[flight[1]]:
+                                if next_flight.arrival_time <= t2:
+                                    flt = list[next_flight.flight_no]
+                                    par = list[flight[1]]
 
-                        for flight in self.graph.adj[curr_flight]:
-                            next_flight_obj = self.all_flights[flight]
-                            next_flight_no = flight
-                            if next_flight_obj.departure_time >= t1 and next_flight_obj.arrival_time <= t2 and cost[next_flight_no][0] > cost[curr_flight][0] + next_flight_obj.fare:
-                                cost[next_flight_no] = (cost[curr_flight][0] + next_flight_obj.fare, self.all_flights[curr_flight])
-                                pq.push((cost[next_flight_no][0], next_flight_no))
+                                    if par[0] + next_flight.fare < flt[0]:
+                                        list[next_flight.flight_no] = (par[0] + next_flight.fare, self.all_flights[flight[1]])
+                                        pq.push((par[0] + next_flight.fare, next_flight.flight_no))
 
-                    a = (1e9, None)      # (cost, flight_object)
-                    if self.city_end[end_city]:
-                        for flgts in self.city_end[end_city]:
-                            if flgts.departure_time >= t1 and flgts.arrival_time <= t2:
-                                if cost[flgts.flight_no][0] < a[0]:
-                                    a = (cost[flgts.flight_no][0], flgts)
+            last_flight = (1e9, None)
+            flt = None
+            for i in self.end_city[end_city]:
+                ft = list[i.flight_no]
+                if ft[0] < last_flight[0]:
+                    last_flight = ft
+                    flt = i
 
-                    if a[1] is not None:
-                        curr_path = []
-                        curr_flgt = a[1]
-                        while cost[curr_flgt.flight_no][1] != 0:
-                            curr_path.append(curr_flgt)
-                            curr_flgt = cost[curr_flgt.flight_no][1]
-                        curr_path.append(curr_flgt)
+            if flt is not None:
+                route.append(flt)
+                while last_flight[1] is not None:
+                    route.append(last_flight[1])
 
-                        curr_path.reverse()
-                        cst1, cst2 = 0, 0
-                        for i in curr_path: cst1 += i.fare
-                        for i in path: cst2 += i.fare
-                        if cst2 == 0 or cst2 > cst1:
-                            path = curr_path
-        return path
+                    last_flight = list[last_flight[1].flight_no]
+
+        return route[::-1]
+
     def least_flights_cheapest_route(self, start_city, end_city, t1, t2):
-        path = []
+        route = []
+        list = [(1e9, 1e9, None) for i in range(self.m + 1)]   # (distance, cost, parent)
 
-        if self.city_start[start_city] is not None and start_city != end_city:
-            for flt in self.city_start[start_city]:
-                if flt.departure_time >= t1 and flt.arrival_time <= t2:
-                    visited, distance = [False] * (self.m + 1), [(0, 0)] * (self.m + 1)  # (distance, previous_flight_object)
-                    visited[flt.flight_no] = True
-                    q = [flt.flight_no]
-
-                    while q:
-                        curr_flight = q.pop(0)
-
-                        for flight in self.graph.adj[curr_flight]:
-                            next_flight_obj = self.all_flights[flight]
-                            next_flight_no = flight
-                            if not visited[next_flight_no] and next_flight_obj.departure_time >= t1 and next_flight_obj.arrival_time <= t2:
-                                visited[next_flight_no] = True
-                                q.append(next_flight_no)
-                                distance[next_flight_no] = (distance[curr_flight][0] + 1, self.all_flights[curr_flight])
-
-                    cost, processed = [(1e9, None)] * (self.m + 1), [False] * (self.m + 1)
+        if start_city != end_city and t1 <= t2:
+            for i in self.start_city[start_city]:
+                if i.departure_time >= t1 and i.arrival_time <= t2:
+                    list[i.flight_no] = (0, i.fare, None)
+                    visited = [False] * (self.m + 1)
                     pq = PriorityQueue()
-                    cost[flt.flight_no] = (0, None)
-                    pq.push((0, flt.flight_no))
+                    pq.push((0, i.fare, i.flight_no))
 
                     while not pq.is_empty():
-                        curr_flight = pq.top()[1]
-                        pq.pop()
+                        flight = pq.pop()
 
-                        if processed[curr_flight]: continue
-                        processed[curr_flight] = True
+                        if not visited[flight[2]]:
+                            visited[flight[2]] = True
+                            for next_flight in self.graph.adj[flight[2]]:
+                                if next_flight.arrival_time <= t2:
+                                    flt = list[next_flight.flight_no]
+                                    par = list[flight[2]]
 
-                        for flight in self.graph.adj[curr_flight]:
-                            next_flight_obj = self.all_flights[flight]
-                            next_flight_no = flight
-                            if next_flight_obj.departure_time >= t1 and next_flight_obj.arrival_time <= t2 and cost[next_flight_no][0] > cost[curr_flight][0] + next_flight_obj.fare and (distance[next_flight_no][0] - distance[curr_flight][0] == 1):
-                                cost[next_flight_no] = (cost[curr_flight][0] + next_flight_obj.fare, self.all_flights[curr_flight])
-                                pq.push((cost[next_flight_no][0], next_flight_no))
+                                    if par[0] + 1 < flt[0]:
+                                        list[next_flight.flight_no] = (par[0] + 1, par[1] + next_flight.fare, self.all_flights[flight[2]])
+                                        pq.push((par[0] + 1, par[1] + next_flight.fare, next_flight.flight_no))
 
-                    a = (1e9, 1e9, None)  # (distance, cost, flight_object)
-                    if self.city_end[end_city]:
-                        for flgts in self.city_end[end_city]:
-                            if flgts.departure_time >= t1 and flgts.arrival_time:
-                                if distance[flgts.flight_no][1]:
-                                    if distance[flgts.flight_no][0] < a[0]:
-                                        a = (distance[flgts.flight_no][0], cost[flgts.flight_no][0], flgts)
-                                    elif distance[flgts.flight_no][0] == a[0] and cost[flgts.flight_no][0] < a[1]:
-                                        a = (distance[flgts.flight_no][0], cost[flgts.flight_no][0], flgts)
-                                elif distance[flgts.flight_no][1] == 0 and flgts.start_city == start_city and cost[flgts.flight_no][0] < a[1]:
-                                    a = (distance[flgts.flight_no][0], cost[flgts.flight_no][0], flgts)
+                                    elif par[0] + 1 == flt[0] and par[1] + next_flight.fare < flt[1]:
+                                        list[next_flight.flight_no] = (flt[0], par[1] + next_flight.fare, self.all_flights[flight[2]])
+                                        pq.push((flt[0], par[1] + next_flight.fare, next_flight.flight_no))
 
-                    if a[2] is not None:
-                        curr_path = []
-                        curr_flgt = a[2]
-                        while curr_flgt is not None:
-                            curr_path.append(curr_flgt)
-                            curr_flgt = cost[curr_flgt.flight_no][1]
+            last_flight = (1e9, 1e9, None)
+            flt = None
+            for i in self.end_city[end_city]:
+                ft = list[i.flight_no]
+                if ft[0] < last_flight[0]:
+                    last_flight = ft
+                    flt = i
+                elif ft[0] == last_flight[0] and ft[1] < last_flight[1]:
+                    last_flight = ft
+                    flt = i
 
-                        curr_path.reverse()
-                        cst1, cst2 = 0, 0
-                        for i in curr_path: cst1 += i.fare
-                        for i in path: cst2 += i.fare
-                        if (len(path) == 0) or len(curr_path) < len(path) or (len(path) == len(curr_path) and cst1 < cst2):
-                            path = curr_path
-        return path
+            if flt is not None:
+                route.append(flt)
+                while last_flight[2] is not None:
+                    route.append(last_flight[2])
+
+                    last_flight = list[last_flight[2].flight_no]
+
+        return route[::-1]
