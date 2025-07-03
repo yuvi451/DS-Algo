f39d126020a3fb9f5a0fd6c3336d43a94ed1ff8f
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2025-07-03 17:35:17 +0530

    Update Kruskal's Algorithm.cpp

--- Graph Algorithms/Kruskal's Algorithm.cpp
+++ Graph Algorithms/Kruskal's Algorithm.cpp
@@ -1,66 +1,76 @@
-class DisjointSet{
-    vl rank, parent, size;
+class DisjointSet {
+    private: 
+        vector<int>rank, size, parent;
+
     public:
-    DisjointSet(int n){
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1, 1);
-        forl(i, 0, n + 1){
-            parent[i] = i;
-        }
-    }
+        DisjointSet(int n){
+            rank.resize(n + 1);
+            size.resize(n + 1);
+            parent.resize(n + 1);
 
-    int findUltPar(int node){
-        if (node == parent[node]) return node;
-        else return parent[node] = findUltPar(parent[node]);
-    }
-
-    void UnionByRank(int u, int v){
-        int ult_u = findUltPar(u);
-        int ult_v = findUltPar(v);
-        if (ult_u == ult_v) return;
-        else if (rank[ult_u] > rank[ult_v]){
-            parent[ult_v] = ult_u;
-        } else if (rank[ult_u] < rank[ult_v]){
-            parent[ult_u] = ult_v;
-        } else {
-            parent[ult_u] = ult_v;
-            rank[ult_v]++;
+            for(int i = 0; i <= n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
         }
-    }
 
-    void UnionBySize(int u, int v){
-        int ult_u = findUltPar(u);
-        int ult_v = findUltPar(v);
-        if (ult_u == ult_v) return;
-        else if (size[ult_u] > size[ult_v]){
-            parent[ult_v] = ult_u;
-            size[ult_u] += size[ult_v];
-        } else if (size[ult_u] < size[ult_v]){
-            parent[ult_u] = ult_v;
-            size[ult_v] += size[ult_u];
-        } else {
-            parent[ult_u] = ult_v;
-            size[ult_v] += size[ult_u];
+        int findPar(int x){
+            if (parent[x] == x) return x;
+            return parent[x] = findPar(parent[x]);
         }
-    }
+
+        void unionByRank(int x, int y){
+            int ut_x = findPar(x);
+            int ut_y = findPar(y);
+            if (ut_x == ut_y) return;
+
+            if (rank[ut_x] < rank[ut_y]){
+                parent[ut_x] = ut_y;
+            } else if (rank[ut_x] > rank[ut_y]){
+                parent[ut_y] = ut_x;
+            } else {
+                parent[ut_x] = ut_y;
+                rank[ut_y]++;
+            }
+        }
+
+        void unionBySize(int x, int y){
+            int ut_x = findPar(x);
+            int ut_y = findPar(y);
+            if (ut_x == ut_y) return;
+
+            if (size[ut_x] < size[ut_y]){
+                parent[ut_x] = ut_y;
+                size[ut_y] += size[ut_x];
+            } else {
+                parent[ut_y] = ut_x;
+                size[ut_x] += size[ut_y];
+            }
+        }
 };
 
-inline bool f(vl a, vl b){
-    if (a[2] < b[2]) return true;
-    else return false;
-}
-
-int KruskalsAlgoritm(vvl edges){
-    sort(all(edges), f);
-    int n = edges.size();
-    DisjointSet ds(n);
-    int mst = 0;
-    fora(x, edges){
-        if (ds.findUltPar(x[0]) != ds.findUltPar(x[1])){
-            ds.UnionByRank(x[0], x[1]);
-            mst += x[2];
+class Solution {
+  public:
+    int spanningTree(int V, vector<vector<int>> adj[]) {
+        vector<vector<int>>edges;
+        for(int i = 0; i < V; i++){
+            for(auto it: adj[i]){
+                int u = i, v = it[0], wt = it[1];
+                edges.push_back({u, v, wt});
+            }
         }
+        sort(edges.begin(), edges.end(), [](vector<int>&a, vector<int>&b) {
+            return a[2] < b[2];
+        });
+        int ans = 0;
+        DisjointSet ds(V);
+        for(auto it: edges){
+            if (ds.findPar(it[0]) != ds.findPar(it[1])){
+                ans += it[2];
+            }
+            ds.unionByRank(it[0], it[1]);
+            
+        }
+        return ans;
     }
-    return mst;
-}
+};
