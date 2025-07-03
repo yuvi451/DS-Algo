bcb7446074dd1336d20e34cf244904c4283631a3
yuvi451 <152682850+yuvi451@users.noreply.github.com>
2025-07-03 17:11:29 +0530

    Update Disjoint Set.cpp

--- Graph Algorithms/Disjoint Set.cpp
+++ Graph Algorithms/Disjoint Set.cpp
@@ -1,49 +1,52 @@
 // Finds whether two elements belong to the same component or not in O(4*alpha) == O(1) time
 // if (ds.findUtlPar(i) == ds.findUltPar(j)) then i & j belong to the same component
-class DisjointSet{
-    vl rank, parent, size;
+class DisjointSet {
+    private: 
+        vector<int>rank, size, parent;
+
     public:
-    DisjointSet(int n){
-        rank.resize(n + 1, 0);
-        parent.resize(n + 1);
-        size.resize(n + 1, 1);
-        forl(i, 0, n + 1){
-            parent[i] = i;
+        DisjointSet(int n){
+            rank.resize(n + 1);
+            size.resize(n + 1);
+            parent.resize(n + 1);
+
+            for(int i = 0; i <= n; i++) {
+                parent[i] = i;
+                size[i] = 1;
+            }
         }
-    }
 
-    int findUltPar(int node){
-        if (node == parent[node]) return node;
-        else return parent[node] = findUltPar(parent[node]);
-    }
-
-    void UnionByRank(int u, int v){
-        int ult_u = findUltPar(u);
-        int ult_v = findUltPar(v);
-        if (ult_u == ult_v) return;
-        else if (rank[ult_u] > rank[ult_v]){
-            parent[ult_v] = ult_u;
-        } else if (rank[ult_u] < rank[ult_v]){
-            parent[ult_u] = ult_v;
-        } else {
-            parent[ult_u] = ult_v;
-            rank[ult_v]++;
+        int findPar(int x){
+            if (parent[x] == x) return x;
+            return parent[x] = findPar(parent[x]);
         }
-    }
 
-    void UnionBySize(int u, int v){
-            int ult_u = findUltPar(u);
-            int ult_v = findUltPar(v);
-            if (ult_u == ult_v) return;
-            else if (size[ult_u] > size[ult_v]){
-                parent[ult_v] = ult_u;
-                size[ult_u] += size[ult_v];
-            } else if (size[ult_u] < size[ult_v]){
-                parent[ult_u] = ult_v;
-                size[ult_v] += size[ult_u];
+        void unionByRank(int x, int y){
+            int ut_x = findPar(x);
+            int ut_y = findPar(y);
+            if (ut_x == ut_y) return;
+
+            if (rank[ut_x] < rank[ut_y]){
+                parent[ut_x] = ut_y;
+            } else if (rank[ut_x] > rank[ut_y]){
+                parent[ut_y] = ut_x;
             } else {
-                parent[ult_u] = ult_v;
-                size[ult_v] += size[ult_u];
+                parent[ut_x] = ut_y;
+                rank[ut_y]++;
+            }
+        }
+
+        void unionBySize(int x, int y){
+            int ut_x = findPar(x);
+            int ut_y = findPar(y);
+            if (ut_x == ut_y) return;
+
+            if (size[ut_x] < size[ut_y]){
+                parent[ut_x] = ut_y;
+                size[ut_y] += size[ut_x];
+            } else {
+                parent[ut_y] = ut_x;
+                size[ut_x] += size[ut_y];
             }
         }
 };
