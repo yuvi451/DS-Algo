7d4bc4639d01a1a97e95af507cdd251d5e0946f2
Yuvraj Singh Sivia <152682850+yuvi451@users.noreply.github.com>
2026-05-26 20:19:30 +0530

    Update Segment Tree.cpp

--- Graph Algorithms/Segment Tree.cpp
+++ Graph Algorithms/Segment Tree.cpp
@@ -1,51 +1,83 @@
-void BUILD(int v, int tl, int tr, vector<int>& sum, vector<int>& arr){
-    // tl and tr are array indices
-    // v is the vertex index: mode for traversing in the tree
+#include <bits/stdc++.h>
+using namespace std;
+#pragma GCC optimize("O3,unroll-loops")
+
+#define ll long long
+#define forl(i, a, b) for(ll i = a; i < b; i++)
+#define rfor(i, a, b) for(ll i = a; i > b; i--)
+#define fora(x, v) for(auto &x : v)
+#define vl vector<ll>
+#define vvl vector<vector<ll>>
+#define pr pair<ll,ll>
+#define pb push_back
+#define vpr vector<pair<ll,ll>>
+#define all(v) v.begin(), v.end()
+
+const ll inf = 1e13;
+const ll mod = 1e9 + 7;
+const ll N = 2e5 + 5;
+
+void BUILD_SUM(vector<int> &arr, vector<int> &t, int v, int tl, int tr){
     if (tl == tr) {
-        sum[v] = arr[tl];
+        t[v] = arr[tl];
     } else {
         int tm = (tl + tr)/2;
-        BUILD(2*v, tl, tm, sum, arr);
-        BUILD(2*v + 1, tm + 1, tr, sum, arr);
-        sum[v] = sum[2*v] + sum[2*v + 1];
+        BUILD_SUM(arr, t, 2*v, tl, tm);
+        BUILD_SUM(arr, t, 2*v + 1, tm + 1, tr);
+        t[v] = t[2*v] + t[2*v + 1];
     }
 }
 
-int SUM(int l, int r, int v, vector<int>& sum, int tl, int tr){
-    // [tl, tr] is segment covered by current vertex v
-    // [l, r] is the segment sum that we have to find
+int SUM(int l, int r, vector<int> &t, int v, int tl, int tr){
     if (l > r) return 0;
 
-    if (l == tl && r == tr) return sum[v];
+    if (tl == l && tr == r) return t[v];
 
     int tm = (tl + tr)/2;
-    // we split query [l, r] into subqueries
-    // intersection between the segment of the query and the segment of the left/right child
-    return SUM(l, min(r, tm), 2*v, sum, tl, tm) + SUM(max(tm + 1, l), r, 2*v + 1, sum, tm + 1, tr);
-
+    return SUM(l, min(tm, r), t, 2*v, tl, tm) + SUM(max(tm + 1, l), r, t, 2*v + 1, tm + 1, tr);
 }
 
-void UPDATE(int v, int tl, int tr, int pos, int value, vector<int>& sum){
-    if (tl == tr){
-        sum[v] = value;
+void UPDATE(int pos, int value, vector<int> &t, int v, int tl, int tr){
+    if (tl == tr) {
+        if (pos == tl) t[v] = value;
     } else {
         int tm = (tl + tr)/2;
         if (pos <= tm){
-            UPDATE(2*v, tl, tm, pos, value, sum);
+            UPDATE(pos, value, t, 2*v, tl, tm);
         } else {
-            UPDATE(2*v + 1, tm + 1, tr, pos, value, sum);
+            UPDATE(pos, value, t, 2*v + 1, tm + 1, tr);
         }
-        sum[v] = sum[2*v] + sum[2*v + 1];
+        t[v] = t[2*v] + t[2*v + 1];
     }
-}   
+}
+
+void BUILD_MAXIMUM(vector<int> &arr, vector<int> &t, int v, int tl, int tr){
+    if (tl == tr) {
+        t[v] = arr[tl];
+    } else {
+        int tm = (tl + tr)/2;
+        BUILD_MAXIMUM(arr, t, 2*v, tl, tm);
+        BUILD_MAXIMUM(arr, t, 2*v + 1, tm + 1, tr);
+        t[v] = max(t[2*v], t[2*v + 1]);
+    }
+}
+
+int MAXIMUM(int l, int r, vector<int> &t, int v, int tl, int tr){
+    if (l > r) return 0;
+
+    if (tl == l && tr == r) return t[v];
+
+    int tm = (tl + tr)/2;
+    return max(MAXIMUM(l, min(tm, r), t, 2*v, tl, tm), MAXIMUM(max(tm + 1, l), r, t, 2*v + 1, tm + 1, tr));
+}
 
 int main(){
     ios::sync_with_stdio(0);
     cin.tie(0);
-    vector<int>arr = {1, 4, -7, 12, 6, -3, 5, 2};
+    vector<int>arr = {2, -5, 6, 8, 12, -7, 5, 3};
     int n = arr.size();
-    vector<int>sum(4*n);
-    BUILD(1, 0, n - 1, sum, arr);
-    cout<<SUM(3, 7, 1, sum, 0, n - 1);
+    vector<int>t(4*n);
+    BUILD_MAXIMUM(arr, t, 1, 0, n - 1);
+    cout<<MAXIMUM(5, 5, t, 1, 0, n - 1);
     return 0;
 }
