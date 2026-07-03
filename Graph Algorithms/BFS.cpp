e00e2ecf1625b855ad94e2efd08a19d2a021d5fc
Yuvraj Singh Sivia <152682850+yuvi451@users.noreply.github.com>
2026-07-03 19:30:43 +0530

    Update BFS.cpp

--- Graph Algorithms/BFS.cpp
+++ Graph Algorithms/BFS.cpp
@@ -1,30 +1,34 @@
-void BFS(vvl adj, vl& ans, int root){
+vector<int> bfs(vector<vector<int>> &adj) {
     int n = adj.size();
-    vl visited(n + 1), distance(n + 1);
-
-    queue <int> q;
-    q.push(root);
+    vector<int>ans, visited(n + 1);
+    queue<int>q;
+    int root = 0;
     visited[root] = 1;
-    ans.pb(root);
-
-    while (!q.empty(){
+    q.push(root);
+    
+    while (!q.empty()){
         int node = q.front();
         q.pop();
-
-        fora(x, adj[node]){
-            if (!(visited[x])){
-                visited[x] = 1;
-                distance[x] = 1 + distance[node];
-                q.push(x);
-                ans.pb(x);
+        ans.push_back(node);
+        
+        for(auto it: adj[node]){
+            if (!visited[it]){
+                visited[it] = 1;
+                q.push(it);
             }
         }
     }
+    return ans;
 }
 
-  //Time Complexity = O(n + m) : every node and edge is visited only once
-  //ans vector stores bfs ordering
-  //BFS is a level by level search algorithm i.e. we first look for all elements that are adjacent to the starting node and then only look for elements 
-  //that are at adjacent to the first neighbours of the starting node [Breadth/Horizontal First Search]
-  //Unlike DFS it picks up a neighbour (adjacent element), visits it and then goes on to the next neighbour (which is also adjacent to the start)
+// Time Complexity = O(n + m) : every node and edge is visited only once
+// ans vector stores BFS ordering
+// BFS is a level by level traversal algorithm i.e. first all nodes that are at level 0 (distance 0 from root i.e. root itself) are processed, then all the nodes at level 1 (distance 1 from root), then level 2 and so on are processed
+// All nodes of a level are processed from left to right
+// Intuititon: intially the queue stores all the nodes of level 0 (root only) then once root is popped level 0 is completely processed; After that we push entire level 1 into the queue (root's children)
+// Because of first in first out property of queue first all the nodes of level 1 get processed then only nodes of level 2 begin getting processed
+// As we pop out nodes of level 1 we push their children i.e. nodes of level 2 into the queue (these obv. get processed after level 1)
+// When entire level 1 is popped out the queue is full of level 2 nodes
+// Finally all the level 2 nodes get processed first before any level 3 nodes processing can begin...
+
   
